e82ea7ae8fe64a60d72666bb06c67016d8a8f100
Júlio <jcaguiarpg@gmail.com>
2023-12-27 10:00:21 -0400

    teste de API GetAll

--- test/manager.api.test.ts
+++ test/manager.api.test.ts
@@ -4,18 +4,6 @@ import mongoose from 'mongoose';
 config();
 
 test('Deve testar o post dos admins da API', async () => {
-  const login = {
-    user: "Júlio",
-    password: "JulinhoFazAPI",
-  }
-  const AxiosLogin = await axios.post(
-    'http://localhost:3000/Login',
-     login,
-  )
-
-  const token = AxiosLogin.data.Token;
-  console.log(token)
-
   const input = {
     name: 'input do post',
     password: '12345678',
@@ -23,34 +11,38 @@ test('Deve testar o post dos admins da API', async () => {
   };
   const AxiosOutput = await axios.post(
     'http://localhost:3000/AdminManagement',
-    input,
-    {
-      headers: {authorization: token}
-    },
+    input
   );
 
   console.log(AxiosOutput.data)
   expect(AxiosOutput.data).toBeDefined();
 }, 300000);
 
-// test('Deve testar o GetAll dos admins da api', async () => {
-//   const input = {
-//     name: 'Input pra deletar',
-//     password: '12345678',
-//     type: 'Servidor da CAED',
-//   };
-//   const AxiosOutput = await axios.post(
-//     'http://localhost:3000/AdminManagement',
-//     input,
-//   );
-//   const AxiosDEL = await axios.delete(
-//     'http://localhost:3000/AdminManagement/'+ AxiosOutput.data.id,
-//     AxiosOutput.data.id,
-//   );
-// }, 15000);
+test('Deve testar o GetAll dos admins da API', async () => {
+  const login = {
+    user: "Júlio",
+    password: "JulinhoFazAPI",
+  }
 
-// const AxiosDEL = await axios.delete(
-//   'http://localhost:3000/AdminManagement/'+ AxiosOutput.data.id,
-//   AxiosOutput.data.id,
-// );
-// }, 15000);
+  const AxiosLogin = await axios.post(
+    'http://localhost:3000/Login',
+     login,
+  )
+  const token = AxiosLogin.data.Token;
+  console.log(token)
+
+  const input = {
+    name: 'Input pra deletar',
+    password: '12345678',
+    type: 'Servidor da CAED',
+  };
+
+  const AxiosOutput = await axios.get(
+    'http://localhost:3000/AdminManagement',
+    {
+      headers: {authorization: token}
+    },
+  );
+
+  expect(AxiosOutput).toBeTruthy();
+}, 15000);

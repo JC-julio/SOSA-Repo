d464bf2e6896553e913871439c212f3e186de44a
Júlio <jcaguiarpg@gmail.com>
2024-01-06 21:37:31 -0400

    rota e controller da entidade de turma corrigidos

--- src/controllers/StudentsManagement/ClassController.ts
+++ src/controllers/StudentsManagement/ClassController.ts
@@ -5,10 +5,9 @@ export default class ClassController {
     static async Post(req: Express.Request, res: Express.Response){
         try{
             const {name} = req.body;
-            const { organizationId } = req.params;
+            const { idOrganization } = req.params;
             console.log(name)
-            const newClass = new StudentClass({name: name, organizationId: organizationId});
-            console.log(newClass)
+            const newClass = new StudentClass({name: name, organizationId: idOrganization});
             const ClassID = (await newClass.Post())._id;
             res.status(201).json({Id: ClassID});
         }catch(error){
@@ -20,6 +19,8 @@ export default class ClassController {
         try{
             const classID = req.params.id;
             const returnClass = await StudentClass.GetOne(classID)
+            if(returnClass.organizationId != req.params.idOrganization)
+                res.status(401).json({msg: 'rota inacessivel'})
             res.status(226).send(returnClass);
         } catch (error) {
             res.status(500).json({ error: error.message });
@@ -29,20 +30,27 @@ export default class ClassController {
     static async GetAll(req: Express.Request, res: Express.Response){
         try{
             const classes = await StudentClass.GetAll();
-            const classesDTO = classes.map((ClassesDTO) => ({
+            const filterClassesByOrganizationId = classes.filter(classes =>
+                req.params.idOrganization.includes(classes.organizationId))
+            if (filterClassesByOrganizationId.length == 0)
+                res.status(404).json({msg: 'nenhuma turma encontrada'})
+            filterClassesByOrganizationId.map((ClassesDTO) => ({
             name: ClassesDTO.name,
             organizationId: ClassesDTO.organizationId,
             id: ClassesDTO.id,
             }))
-            res.status(226).json(classesDTO);
+            res.status(226).json(filterClassesByOrganizationId);
         }catch(error){
             res.status(500).json({ error: error.message });
     }
 }
 
     static async Delete(req: Express.Request, res: Express.Response){
-        const ClassID = req.params.id;
         try{
+        const ClassID = req.params.id;
+        const GetOneClass = await StudentClass.GetOne(ClassID);
+        if(GetOneClass.organizationId != req.params.idOrganization)
+            res.status(401).json({msg: 'rota inacessivel'})
             await StudentClass.Delete(ClassID);
             res.status(200).end();
         } catch(error) {
--- src/routes/StudentsRoutes/RoutesClass.ts
+++ src/routes/StudentsRoutes/RoutesClass.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import ClassController from '../../controllers/StudentsManagement/ClassController';
 import { organizationRequired } from '../../middlewares/middlewareDeOrganization';
-//ALL TESTED
+import { loginRequired } from '../../middlewares/middlewareDeLogin';
+//ALL right
 
 const router = express.Router();
 
-router.post('/Organization/:idOrganization/Class', organizationRequired, ClassController.Post);
+router.post('/Organization/:idOrganization/Class', loginRequired, ClassController.Post);
 
-router.get('/Organization/:idOrganization/Class/:id', organizationRequired, ClassController.GetOne);
+router.get('/Organization/:idOrganization/Class/:id', loginRequired, ClassController.GetOne);
 
-router.get('/Organization/:idOrganization/Class', organizationRequired, ClassController.GetAll);
+router.get('/Organization/:idOrganization/Class', loginRequired, ClassController.GetAll);
 
-router.delete('/Organization/:idOrganization/Class/:id', organizationRequired, ClassController.Delete);
+router.delete('/Organization/:idOrganization/Class/:id', loginRequired, ClassController.Delete);
 
 export default router;
